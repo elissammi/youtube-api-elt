973e37d0e59ff4bb3eee17e052a763f57652da07
Sammi C <sammichiali@gmail.com>
2026-04-02 17:29:02 +1100

    dwh

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,13 +1,19 @@
 import requests, json,os
 from dotenv import load_dotenv
 from datetime import date
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
 
+# load_dotenv(dotenv_path="./.env")
+# API_KEY = os.getenv("API_KEY")
+# CHANNEL_HANDLE = "MrBeast"
+
+API_KEY= Variable.get("API_KEY")
+CHANNEL_HANDLE= Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
 
     try:
@@ -27,7 +33,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
-
+@task
 def get_video_ids(playlistId):
     video_ids = []
     pageToken=None
@@ -57,7 +63,8 @@ def get_video_ids(playlistId):
     
     except requests.exceptions.RequestException as e:
         raise e
-
+    
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -98,6 +105,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
     
+@task    
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
